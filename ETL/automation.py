29423d4e5fff035b0ff539b09e56debcf13a3d1c
Mouloud <mouloud.bellil@outlook.fr>
2025-11-25 17:50:17 +0100

    add automation

--- ETL/automation.py
+++ ETL/automation.py
@@ -10,89 +10,72 @@
 # The function get_last_rowid must return the last rowid of the table sales_data on the IBM DB2 database or PostgreSql.
 
 import os
-import mysql.connector
-import psycopg2
+from typing import List
 from dotenv import load_dotenv
+from logging import Logger
+
+
+from utils.db_connection import (
+    create_mysql_connection,
+    create_postgres_connection,
+    insert_to_database,
+    query_database,
+)
 
 # Load environment variables
 load_dotenv()
 
 
-def get_last_rowid():
-	conn = psycopg2.connect(
-		database = os.environ["dsn_database"], 
-		user = os.environ["dsn_user"],
-		password = os.environ["dsn_pwd"],
-		host = os.environ["dsn_hostname"], 
-		port = os.environ["dsn_port"]
-	)
-	
-	cursor = conn.cursor()
-	sql_statement = "SELECT rowid FROM sales_data ORDER BY rowid DESC LIMIT 1"
-	cursor.execute(sql_statement)
-	last_row = cursor.fetchone()
+def get_last_rowid(logger: Logger):
+    """summary"""
+    conn = create_postgres_connection(logger)
+    if conn == -1:
+        os._exit(1)
 
-	conn.close()
-	return last_row[0]
+    sql_statement = "SELECT rowid FROM sales_data ORDER BY rowid DESC LIMIT 1"
+    last_rowid = query_database(conn, sql_statement, logger)[0]
+    if last_rowid == -1:
+        os._exit(1)
 
-last_row_id = get_last_rowid()
-print("Last row id on production datawarehouse = ", last_row_id)
+    logger.info("Last row id on production datawarehouse")
+    logger.info(last_rowid)
+
+    return last_rowid
+
 
 # List out all records in MySQL database with rowid greater than the one on the Data warehouse
 # The function get_latest_records must return a list of all records that have a rowid greater than the last_row_id in the sales_data table in the sales database on the MySQL staging data warehouse.
 
-def get_latest_records(rowid):
-	conn = mysql.connector.connect(
-		user='root',
-		password=os.environ["DB_PASSWORD"],
-		host=os.environ["DB_HOST"],
-		database='sales'
-    )
-	
-	cursor = conn.cursor()
-	
-	sql_statement = f"SELECT * FROM sales_data WHERE rowid > {rowid}"
-	cursor.execute(sql_statement)
-	records = cursor.fetchall()
-	
-	conn.close()
-	return  records
- 
 
-new_records = get_latest_records(last_row_id)
+def get_latest_records(rowid: int, logger: Logger):
+    conn = create_mysql_connection(logger)
+    if conn == -1:
+        os._exit(1)
 
-print("New rows on staging datawarehouse = ", new_records)
+    sql_statement = f"SELECT * FROM sales_data WHERE rowid > {rowid}"
+    records = query_database(conn, sql_statement, logger)
+    if not records:
+        logger.info("No new recordings!")
+        os._exit(1)
 
-# Insert the additional records from MySQL into DB2 or PostgreSql data warehouse.
+    logger.info("Number new rows on staging datawarehouse")
+    logger.info(len(records))
+
+    return records
+
+
 # The function insert_records must insert all the records passed to it into the sales_data table in IBM DB2 database or PostgreSql.
 
-def insert_records(records):
-    
-	if not records:
-		print("No new recordings!")
-		os._exit(1)
 
-	conn = psycopg2.connect(
-		database = os.environ["dsn_database"], 
-		user = os.environ["dsn_user"],
-		password = os.environ["dsn_pwd"],
-		host = os.environ["dsn_hostname"], 
-		port = os.environ["dsn_port"]
-	)
-	
-	cursor = conn.cursor()
-	
-	for record in records:
-		sql_statement = f"INSERT INTO sales_data(rowid, product_id, customer_id, quantity) VALUES {record}" 
-		cursor.execute(sql_statement)
-		conn.commit()
-	conn.close()
+def insert_records(records: List, logger: Logger):
 
-insert_records(new_records)
-print("New rows inserted into production datawarehouse = ", len(new_records))
+    conn = create_postgres_connection(logger)
 
-# disconnect from mysql warehouse
+    for record in records:
+        sql_statement = f"INSERT INTO sales_data(rowid, product_id, customer_id, quantity) VALUES {record}"
+        insert_to_database(conn, sql_statement, logger)
 
-# disconnect from DB2 or PostgreSql data warehouse 
+    logger.info("Number new rows inserted into production datawarehouse")
+    logger.info(len(records))
 
-# End of program
+    conn.close()
